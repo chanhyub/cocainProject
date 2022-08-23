72a10bff8179eb5c9967ff71ecc3772083e3d068
ysh <dbtmdgks7897@outlook.kr>
2022-08-23 15:28:19 +0900

    html 작업

--- src/main/java/com/cocain/cocainproject/CocainProjectApplication.java
+++ src/main/java/com/cocain/cocainproject/CocainProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CocainProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CocainProjectApplication.class, args);
     }
--- src/main/java/com/cocain/cocainproject/domain/TbUser.java
+++ src/main/java/com/cocain/cocainproject/domain/TbUser.java
@@ -5,11 +5,22 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @NoArgsConstructor
 @Setter
 @Getter
 @Entity
-public class TbUser {
+public class TbUser{
 
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
