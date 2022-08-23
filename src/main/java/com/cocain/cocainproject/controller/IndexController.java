d79ab65aced23e6deb7f0525b70dc35051e60e12
chan <chan991028@naver.com>
2022-08-23 17:00:35 +0900

    Sns 로그인 (google, naver) 구현

--- src/main/java/com/cocain/cocainproject/controller/IndexController.java
+++ src/main/java/com/cocain/cocainproject/controller/IndexController.java
@@ -1,14 +1,24 @@
 package com.cocain.cocainproject.controller;
 
+import com.cocain.cocainproject.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // (1)
+        if(user != null) { // (2)
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
